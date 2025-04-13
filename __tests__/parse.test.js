f399db5c71ade3dfa5cb24e214ccf60fbaf86f05
Addam Shover <157143242+Shadowing122@users.noreply.github.com>
2025-04-13 19:13:43 -0400

    Update parse.test.js

--- __tests__/parse.test.js
+++ __tests__/parse.test.js
@@ -1,12 +1,12 @@
 import { parse } from '../WebsiteStuff/CSVparse.js';
 
 test('parse ruturns an array', () => {
-    const data = parse();
+    const data = await parse();
     expect(Array.isArray(data)).toBe(true);
 });
 
 test('parse returns an array of arrays', () => {
-    const data = parse();
+    const data = await parse();
     expect(data.length).toBeGreaterThan(0);
     data.forEach(row => {
         expect(Array.isArray(row)).toBe(true);
