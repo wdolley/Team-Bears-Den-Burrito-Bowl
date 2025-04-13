501833f2934c7b9228a1892322c47cb8226886e9
William Dolley <157143248+wdolley@users.noreply.github.com>
2025-04-13 19:56:37 -0400

    Update buttonHandler.js

--- buttonHandler.js
+++ buttonHandler.js
@@ -2,6 +2,12 @@ function handleClick() {
   console.log("Button was clicked!");
 }
 
-document.getElementById("generate-btn").addEventListener("click", handleClick);
+// Setup function to attach the handler
+function attachClickHandler() {
+  const button = document.getElementById("generate-btn");
+  if (button) {
+    button.addEventListener("click", handleClick);
+  }
+}
 
-module.exports = { handleClick };
+module.exports = { handleClick, attachClickHandler };
