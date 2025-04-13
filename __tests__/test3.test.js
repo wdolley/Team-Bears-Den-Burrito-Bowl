b3db7cdc004a8328aff242d1cc976151016af679
William Dolley <157143248+wdolley@users.noreply.github.com>
2025-04-13 19:49:50 -0400

    Update test3.test.js

--- __tests__/test3.test.js
+++ __tests__/test3.test.js
@@ -2,21 +2,18 @@ const { handleClick } = require('./buttonHandler');
 
 describe('Button click', () => {
   beforeEach(() => {
-    // Set up our document body
+    // Set up the DOM
     document.body.innerHTML = `<button id="myButton">Click Me</button>`;
     document.getElementById('myButton').addEventListener('click', handleClick);
   });
 
-  it('calls handleClick on button click', () => {
-    // Spy on handleClick
+  test('calls handleClick on button click', () => {
     const clickSpy = jest.spyOn(console, 'log');
 
-    // Simulate the click
     document.getElementById('myButton').click();
 
     expect(clickSpy).toHaveBeenCalledWith('Button was clicked!');
     
-    // Clean up
     clickSpy.mockRestore();
   });
 });
