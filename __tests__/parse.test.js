9a1210f20c899f41cd7f3a53beaa6100d8883158
Addam Shover <157143242+Shadowing122@users.noreply.github.com>
2025-04-13 17:41:34 -0400

    Add files via upload

---
+++ __tests__/parse.test.js
@@ -0,0 +1,17 @@
+import { parse } from './WebsiteStuff/CSVparse.js';
+
+test('parse ruturns an array', () => {
+    const data = parse();
+    expect(Array.isArray(data)).toBe(true);
+});
+
+test('parse returns an array of arrays', () => {
+    const data = parse();
+    expect(data.length).toBeGreaterThan(0);
+    data.forEach(row => {
+        expect(Array.isArray(row)).toBe(true);
+    });
+}
+);
+
+
