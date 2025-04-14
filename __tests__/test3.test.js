aac1a34217235b799f826a695050de4c0c7f3805
William Dolley <157143248+wdolley@users.noreply.github.com>
2025-04-13 20:40:31 -0400

    Update test3.test.js

--- __tests__/test3.test.js
+++ __tests__/test3.test.js
@@ -1,31 +1,21 @@
 /**
  * @jest-environment jsdom
  */
-
 const { handleClick, attachClickHandler } = require('../buttonHandler');
 
 describe('Button click', () => {
   beforeEach(() => {
-    document.body.innerHTML = `
-      <button id="generate-btn">Generate</button>
-      <button id="submit">Login</button>
-    `;
+    document.body.innerHTML = <button id="generate-btn">Generate</button>;
     attachClickHandler();
   });
 
-  test('calls handleClick on generate button click', () => {
+  test('calls handleClick on button click', () => {
     const spy = jest.spyOn(console, 'log');
+
     document.getElementById('generate-btn').click();
-    expect(spy).toHaveBeenCalledWith('generate-btn was clicked!');
+
+    expect(spy).toHaveBeenCalledWith('Button was clicked!');
+
     spy.mockRestore();
   });
-
-  test('calls handleClick on submit button click', () => {
-    const spy = jest.spyOn(console, 'log');
-    document.getElementById('submit-btn').click();
-    expect(spy).toHaveBeenCalledWith('submit-btn was clicked!');
-    spy.mockRestore();
-  });
-
-});
-
+}); 
